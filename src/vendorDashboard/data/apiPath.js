ff302d011d61f5d6de7d8bab4c19981387a17090
Divya Harika <divyaharika24@gmail.com>
2025-07-30 20:27:04 +0530

    changed in path to live in api url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:4000"
-// const API_URI = "https://backend-nodejs-t6y5.onrender.com"
+//export const API_URL = "http://localhost:4000"
+const API_URI = "https://backend-nodejs-t6y5.onrender.com"
